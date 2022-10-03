f22ffd822a67b88f1b5d02e230aa4dd10c7105fd
user <user@sitonOS.local.lan>
2022-10-03 11:09:36 +0800

    add test hpatches part

---
+++ benchmark_test/test_utils.py
@@ -0,0 +1,25 @@
+def create_results():
+       return {
+       'num_features': [],
+       'rep_single_scale': [],
+       'rep_multi_scale': [],
+       'num_points_single_scale': [],
+       'num_points_multi_scale': [],
+       'error_overlap_single_scale': [],
+       'error_overlap_multi_scale': [],
+       'mma': [],
+       'mma_corr': [],
+       'num_matches': [],
+       'num_mutual_corresp': [],
+       'avg_mma': [],
+       'num_matches': [],
+    }
+
+def create_metrics_results(sequences, top_k, overlap, pixel_threshold):
+    
+    results = create_results()
+    results['sequences'] = sequences
+    results['top_k'] = top_k
+    results['overlap'] = overlap
+    results['pixel_threshold'] = pixel_threshold
+    return results
